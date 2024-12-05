37eb1b4535c67ebc2923d072f95e054aa3d9155b
antoniotrancoso01 <al2024116930@alunos.utad.pt>
2024-12-05 11:57:32 +0000

    att

--- Assets/seguirOplayer.cs
+++ Assets/seguirOplayer.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class seguirOplayer : MonoBehaviour
 {
+    public Animator Anim;
     public Transform Target;
-    public float speed=4f;
+    public float speed=2f;
+    public float detectionRange = 1f;
     Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +17,20 @@ public class seguirOplayer : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 pos=Vector3.MoveTowards(transform.position,Target.position,speed*Time.fixedDeltaTime);
+        // Calcula a distância entre o inimigo e o jogador
+        float distanceToTarget = Vector3.Distance(transform.position, Target.position);
+
+        // Atualiza o parâmetro do Animator com base na distância
+        if (distanceToTarget <= detectionRange)
+        {
+           Anim.SetBool("Isperto", true);
+        }
+        else
+        {
+            Anim.SetBool("Isperto", false);
+        }
+
+        Vector3 pos =Vector3.MoveTowards(transform.position,Target.position,speed*Time.fixedDeltaTime);
         rb.MovePosition(pos);
         transform.LookAt(Target);
     }
